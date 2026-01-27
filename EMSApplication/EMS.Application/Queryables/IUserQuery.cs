ab33a868e3f6293e85f669e57d9a62ce3b2cbe99
rubin.m <rubin.m@cantier.com>
2026-01-27 12:45:46 +0530

    Queryable logic added changes committed by rubin

---
+++ EMSApplication/EMS.Application/Queryables/IUserQuery.cs
@@ -0,0 +1,10 @@
+﻿using EMS.Domain.Entities;
+using EMS.Infrastructure.Persistence.Interface;
+
+namespace EMS.Application.Queryables
+{
+    public interface IUserQuery : IQueryBuilder<User, int>
+    {
+        IUserQuery WhereIdIs(int id);
+    }
+}
---
+++ EMSApplication/EMS.Logic/Queryables/UserQuery.cs
@@ -0,0 +1,21 @@
+﻿using EMS.Application.Queryables;
+using EMS.Domain.Entities;
+using EMS.Infrastructure.Persistence.Interface;
+using EMS.Infrastructure.Persistence.Service;
+
+namespace EMS.Logic.Queryables
+{
+    public class UserQuery : QueryBuilder<User, int>, IUserQuery
+    {
+        public UserQuery(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+
+        public IUserQuery WhereIdIs(int id)
+        {
+            Where(x => x.Id == id);
+            return this;
+        }
+    }
+
+}
